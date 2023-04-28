e34a84f2c51b4d3ba63f49ea7cc034396e0c5fd5
crepybozz <mungthomte@gmail.com>
2023-04-28 17:17:03 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+$('.collapse').on("click", () => {
+          $('.xmark').toggle("hidden")
+          $('.collapse').toggle("hidden")
+          $('.nav-popup').toggle("hidden")
+})
+
+$('.xmark').on('click', () => {
+          $('.xmark').toggle("hidden")
+          $('.collapse').toggle("hidden")
+          $('.nav-popup').toggle("hidden")
+})
